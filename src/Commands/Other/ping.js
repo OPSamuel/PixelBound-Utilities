f62a9ac075f1b84417a790754219ef353c46f08b
OP SamuelCC <80846151+OPSamuel@users.noreply.github.com>
2025-03-24 17:30:02 +0000

    Add files via upload

--- src/Commands/Other/ping.js
+++ src/Commands/Other/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder} = require("discord.js")
 
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Tests if the bot is currently responding."),
+
+    async execute (interaction) {
+        interaction.reply({content: "Pong", ephemeral: true})
+    }
+}
