d6a40471b030927751e57dc979e4b130afb3aa90
OP SamuelCC <80846151+OPSamuel@users.noreply.github.com>
2025-04-14 17:05:09 +0100

    Add files via upload

--- src/data/Webhooks.js
+++ src/data/Webhooks.js
@@ -1,7 +1,6 @@
 module.exports = {
+    error: 'https://discord.com/api/webhooks/1361040391255625798/9Juk_jEbV_ry_uXkKsYs3ZTCQ16lE7x428BJpH-g4S5Y39tlU0ohCuxLzjrwv3JSbm7D',
     logs: 'https://discord.com/api/webhooks/1358814852747100351/IMMm5E8OWwk3WpOaC4LY0pRQAnpVr4H_gc6cxY1hlUkT6-wqDZTNJrbpjktdGLcMUaMx',
     NewGuildMember: 'https://discord.com/api/webhooks/1358857761374339273/wY_arUH3XuqM0eV9eBMruIhO_STh8iBtxBFraxoIKW0wQIK1RJcYbt_K1_-J5XrpJCpQ',
     messages: 'https://discord.com/api/webhooks/1358867251331076209/_H0WKbGWUu4t4qZ7QrY6CuzJe185-fQ1Es6X3cEuBDnSqkR33TRiVXCaSdKHSvLnKgh8',
-    roles: 'https://discord.com/api/webhooks/1359308822996324352/f525yemTBfe0--KkjV_tTDrzJYQoeJu_Oke2q5weKUY8xSIeuvcXCSBw0wM4SbWEI2mA',
-    channels: 'https://discord.com/api/webhooks/1359310326541062204/0YHaIQ_48VwJnVyAjcuD43F4gmW-BnVykSjs_iAZS2uae8nkajyP7ufC9uuYFpd_wI6n',
 };
